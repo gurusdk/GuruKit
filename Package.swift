9ba3066330f6f3dbb8640a638f962915bbc34029
Alan <alanbyte@outlook.com>
2025-02-26 11:05:27 +0700

    chore(tag): v0.0.3

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
         ),
     ],
     dependencies: [
-        .package(url: "https://github.com/gurusdk/GuruKit.git", from: "0.0.2"),
+        .package(url: "https://github.com/gurusdk/GuruKit.git", from: "0.0.3"),
     ],
     targets: [
         .binaryTarget(
